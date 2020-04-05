92017a0d07f025c1d11a248df684b9216b72303a
crispy2606 <crispy2606@gmx.at>
2020-04-05 15:25:09 +0200

    first build & testing spdlog

---
+++ Code/cristian/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "asio.hpp"
+#include "spdlog/spdlog.h"
+#include "CLI11.hpp"
+
+using namespace std;
+
+int main(){
+    cout << "Cristian" << endl;
+    spdlog::info("Cristian");
+}
