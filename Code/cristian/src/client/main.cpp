1e704787a61c106e06dbf1ad27ac732919680e62
crispy2606 <crispy2606@gmx.at>
2020-04-09 00:47:54 +0200

    add client code

---
+++ Code/cristian/src/client/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "asio.hpp"
+#include "spdlog/spdlog.h"
+#include "CLI11.hpp"
+
+using namespace std;
+
+int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[]){
+
+    tcp::iostream strm{"localhost", "1000"};
+
+    if(strm){
+        string data;
+        getline(strm, data);
+        cout << data << endl;
+        strm.close();
+    }else{
+        cerr << "Could not connect to Server." << endl;
+    }
+
+    return EXIT_SUCCESS;
+}
