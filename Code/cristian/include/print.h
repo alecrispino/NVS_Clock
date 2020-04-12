eda787471af59783a6b9f310083682f35ddd7dcc
crispy2606 <crispy2606@gmx.at>
2020-04-12 13:52:00 +0200

    print time with spdlog

--- Code/cristian/include/print.h
+++ Code/cristian/include/print.h
@@ -11,10 +11,17 @@ inline std::ostream& operator<<(std::ostream& out, decltype(std::chrono::system_
     return out;
 }
 
+std::string to_string(std::chrono::system_clock::time_point tp){
+    std::stringstream sstream;
+    sstream << tp;
+    return sstream.str();
+}
+
 void printer(std::chrono::system_clock::time_point time){
     //std::time_t t{std::chrono::system_clock::to_time_t(time)};
     std::lock_guard<std::mutex> lg{mtx};
-    std::cout << time << std::endl;
+    //std::cout << time << std::endl;
+    spdlog::info(to_string(time));
 }
 
 void printer(std::string msg){
